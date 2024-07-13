ff5547041afa42ebc392737fdc9d57c2625c1be2
Prabel <120583804+HiPrabel@users.noreply.github.com>
2024-07-13 15:25:35 +0000

    day one done

---
+++ one.js
@@ -0,0 +1,33 @@
+var one = 1
+console.log(one);
+
+let two = 'hello'
+console.log(two);
+
+const value = true
+console.log(value);
+
+let three = 3
+let four = 'four'
+let five = false
+let six = [1,2,3,4,5]
+let seven = {name: 'kunal', age: 20, friends: ['rahul', 'vivek', 'raj']}
+
+
+console.log(typeof three);
+console.log(typeof four);
+console.log(typeof five);
+console.log(typeof six);
+console.log(typeof seven);
+console.log(Array.isArray(six));
+console.log(Array.isArray(seven));
+
+let eight = 8
+console.log(eight);
+eight = 9
+console.log(eight);
+
+const nine = 9
+console.log(nine);
+// nine = 10
+// TypeError: Assignment to constant variable.
