684314ca5025f77d4c9848b38dee7d19a4327fc5
Prabel <120583804+HiPrabel@users.noreply.github.com>
2024-07-16 14:21:27 +0000

    day four done

---
+++ four.js
@@ -0,0 +1,72 @@
+
+// Task 1
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(i);    
+// }
+
+// Task 2
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(`5 x ${i} = ${5*i}`);
+// }
+
+// Task 3
+
+// let i = 1
+// let total = 0
+// while (i<11) {
+//     total += i
+//     i++
+// }
+// console.log(total);
+
+// Task 4
+
+// let i = 10
+// while (i>0) {
+//     console.log(i);
+//     i--
+// }
+
+// Task 5
+
+// let i = 1
+// do {
+//     console.log(i);
+//     i++
+// } while (i<6);
+
+// Task 6
+
+// let n = 5
+// let fact = 1
+// do {
+//     fact *= n
+//     n--
+// } while (n>0);
+// console.log(fact);
+
+// Task 7
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = '';
+//     for (let j = 1; j <= i; j++) {
+//         row += '* ';
+//     }
+//     console.log(row);
+// }
+
+// Task 8
+
+// for (let i = 1; i < 11; i++){
+//     if (i == 5) continue;
+//     console.log(i);
+// }
+
+// Task 9
+
+for (let i = 1; i < 11; i++){
+    if (i == 7) break;
+    console.log(i);
+}
