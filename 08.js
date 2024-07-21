f90688cf1dd6c7e83b9dba673e9f9c8d93c22bd6
Prabel <120583804+HiPrabel@users.noreply.github.com>
2024-07-21 15:57:52 +0000

    day nine done

--- 08.js
+++ 08.js
@@ -1,86 +1,88 @@
-// Task 1
-let name = 'jaypraksh'
-let age = 19
+// // Task 1
+// let name = 'Prabel'
+// let age = 19
 
-let person = `Hello, my name is ${name} and I am ${age} years old.`
-console.log(person);
+// let intro = `Hello, my name is ${name} and I am ${age} years old.`
+// console.log(intro);
 
-// Task 2
-let multiLineStr = `This is multiline string.
-This is 2st line.
-This is 3rd line.
+// // Task 2
+// let multiLineStr = `This is multiline string.
+// This is 2nd line.
+// This is 3rd line.
 
-`
-console.log(multiLineStr);
+// `
+// console.log(multiLineStr);
 
 // Task 3
 
-let numbers = [1, 2, 3 ,4];
+// let numbers = [5, 2, 8 ,3];
 
-let [a, b] = numbers;
+// let [a, b] = numbers;
 
-console.log(a);
-console.log(b)
+// console.log('a = ',a);
+// console.log('b = ',b)
 
 // Task 4
 
-let book = {
-    title: 'how to become coding expert',
-    author: 'jaypraksh'
-}
+// let book = {
+//     title: 'book1',
+//     author: 'kunal'
+// }
 
-let {title, author} = book
+// let {title, author} = book
 
-console.log(title);
-console.log(author);
+// console.log(title);
+// console.log(author);
 
 // Task 5
 
-let existngArray = [1,2,3,4]
-let addtionalArray = [5, 6, 7, 8, 9]
+// let existngArray = [1,2,3,4]
+// let addtionalArray = [5, 6, 7, 8, 9]
 
-let newArray = [...existngArray, ...addtionalArray]
-console.log(newArray);
+// let newArray = [ ...addtionalArray, ...existngArray]
+// console.log(newArray);
  
 // Task 6
 
-let sumAll = function(...numbers){
-    return numbers.reduce((acc, cur) => acc + cur, 0);
-}
+// let sumAll = function(...numbers){
+//     return numbers.reduce((acc, cur) => acc + cur, 0);
+// }
 
-console.log(sumAll(1, 2, 3, 4, 5));
+// console.log(sumAll(1, 2, 3, 4, 5));
 
-// Task 7
+// // Task 7
 
-let multiply = function (num1, num2 = 2){
-    return num1 * num2
-}
+// let multiply = function (num1, num2 = 2){
+//     return num1 * num2
+// }
 
-console.log(multiply(4, 5));
-console.log(multiply(5));
+// console.log(multiply(4, 5));
+// console.log(multiply(5));
 
-// Task 8
+// // Task 8
 
-let name2 = 'jay';
-const age2  = 19;
+// let name = 'rahul';
+// const age  = 19;
 
-let person2 = {
-    name2,
-    age2
-}
+// let person = {
+//     name,
+//     age
+// }
+
+// console.log(person);
 
 
 // Task 9
 
-const propName1 = 'firstName';
-const propName2 = 'lastName';
-const propName3 = 'age';
+const prop1 = 'firstName';
+const prop2 = 'lastName';
+const prop3 = 'age';
 
 // Creating an object with computed property names
-const person5 = {
-  [propName1]: 'priya',
-  [propName2]: 'Dey',
-  [propName3]: 30
+const person = {
+  [prop1]: 'rahul',
+  [prop2]: 'kunal',
+  [prop3]: 12.4
 };
 
-console.log(person5);
+console.log(person);
